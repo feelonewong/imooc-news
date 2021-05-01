d3d18f4754305627c3319d8109b5bd4d7b27aea1
feelonewong <feelonewong@gmail.com>
2021-05-02 07:00:31 +0800

    初始化云数据库空间，初始化tabbar配置

---
+++ pages/tabbar/follow/follow.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		follow页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
