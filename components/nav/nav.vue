ad02312e36fea6e54333ff2056d8f1fff41eb7ed
feelonewong <feelonewong@gmail.com>
2021-05-02 10:56:33 +0800

    根据不同设备设置导航栏高度

---
+++ components/nav/nav.vue
@@ -0,0 +1,96 @@
+<template>
+	<view class="nav">
+		<view class="nav-fixed">
+			<!-- 状态栏高度 -->
+			<view :style="{height: statusHeight+'px'}"></view>
+			<!-- searchbar内容 -->
+			<view class="nav-content" :style="{height: searchHeight+'px',
+				width: searchWidth+'px'
+			}">
+				<view class="nav-search">
+					<view class="nav-search_icon"></view>
+					<view class="nav-search_text">
+						uniapp、vue
+					</view>
+				</view>
+			</view>
+
+		</view>
+		<view :style="{height: statusHeight+searchHeight+'px'}"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				statusHeight: 0,
+				searchHeight: 45,
+				searchWidth: 375
+			}
+		},
+		created() {
+			
+			//获取状态栏高度
+			let info = uni.getSystemInfoSync()
+			this.statusHeight = info.statusBarHeight;
+			this.searchWidth = info.windowWidth;
+			//计算胶囊高度设置为search的高度
+			//公式:(胶囊底部高度-statusBarHeight高度)+(胶囊顶部高度-statusBarht高度) ===胶囊本身高度
+			// 等价于胶囊本身高度和上边边距 + 下边边距
+			// #ifdef MP-WEIXIN
+			const menuButtonInfo = uni.getMenuButtonBoundingClientRect();
+			let height = (menuButtonInfo.bottom - info.statusBarHeight) +
+				(menuButtonInfo.top - info.statusBarHeight);
+			this.searchHeight = height;
+			this.searchWidth = menuButtonInfo.left;
+			// #endif
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.nav {
+		.nav-fixed {
+			position: fixed;
+			top: 0;
+			left: 0;
+			background-color: $mk-base-color;
+			width: 100%;
+
+			.nav-content {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				box-sizing: border-box;
+
+				padding: 0 15px;
+
+				.nav-search {
+					height: 30px;
+					width: 100%;
+					display: flex;
+					align-items: center;
+					border-radius: 30px;
+					padding: 0 15px;
+					background-color: #fff;
+
+					.nav-search_icon {
+						width: 10px;
+						height: 10px;
+						border: 1px solid red;
+						margin-right: 10px;
+					}
+
+					.nave-search_text {
+						font-size: 12px;
+						color: #999;
+					}
+				}
+			}
+		}
+	}
+</style>
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,22 +1,26 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-			主页面
+		<Nav></Nav>
+		<view  v-for="(item,index) in 100" :key="index">
+				{{item}}--正文
 		</view>
+		
 	</view>
 </template>
 
 <script>
+	import Nav from "@/components/nav/nav.vue"
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: "hello"
 			}
 		},
 		onLoad() {
 
+		},
+		components:{
+			Nav:Nav
 		},
 		methods: {
 
@@ -25,29 +29,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
 </style>
